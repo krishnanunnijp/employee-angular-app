b2828677ffe3501d40e1e05365ccfd5b3b562039
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-24 16:41:02 +0530

    employee edit added

---
+++ src/app/employeeedit/employeeedit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employeeedit',
+  templateUrl: './employeeedit.component.html',
+  styleUrls: ['./employeeedit.component.css']
+})
+export class EmployeeeditComponent {
+
+}
