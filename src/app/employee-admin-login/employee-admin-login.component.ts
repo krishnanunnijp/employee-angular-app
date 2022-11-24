340c5a05969a6302153bada734c2e927d5b8fe44
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-24 16:25:59 +0530

    admin page added

---
+++ src/app/employee-admin-login/employee-admin-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-admin-login',
+  templateUrl: './employee-admin-login.component.html',
+  styleUrls: ['./employee-admin-login.component.css']
+})
+export class EmployeeAdminLoginComponent {
+
+}
