18d9087c8b28b2e63723a11e9534f996bf63dd58
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 11:00:30 +0530

    databinding added

--- src/app/employee-view/employee-view.component.ts
+++ src/app/employee-view/employee-view.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee-view.component.css']
 })
 export class EmployeeViewComponent {
+  data:any = [
+    {
+      name: "krishnanunni",
+      phone: 8848710090,
+      email: "krishnanunnijpjaya@gmail.com",
+      designation: "software development trainee",
+      salary: 24137125
+    },
+    {
+      name: "sachin",
+      phone: 48753849,
+      email: "sachin@gmail.com",
+      designation: "software development trainee",
+      salary: 2537636
+    },
+    {
+      name: "dfgf",
+      phone: 345526554,
+      email: "thdtyhyt",
+      designation: "rthrth",
+      salary: 2222222222265
+    },
+    {
+      name: "fgdhdhdhdhdhdhfg",
+      phone: 8848710090,
+      email: "dhrthdhty",
+      designation: "software development trainee",
+      salary: 24137125
+    },
+    {
+      name: "fgbhdfbcfg",
+      phone: 4625656546,
+      email: "sdgfbfgbdfgb",
+      designation: "sshstrhtrhrd",
+      salary: 5463565
+    }
 
+  ]
 }
